b27d7529f7ccec2a9e2faa9eceadb7af6551d020
mmidtbo <mauza628@gmail.com>
2026-05-30 00:45:50 +0700

    fix(js): fix button theme logic and naming convention

--- script/include.js
+++ script/include.js
@@ -1,15 +1,24 @@
+import initThemeToggle from "./app-jsdom.js";
+
 async function loadComponent(id, file) {
   const element = document.getElementById(id);
   const response = await fetch(file);
   const html = await response.text();
   element.innerHTML = html;
 }
+async function InitPage() {
+  await Promise.all([
+    loadComponent("navbar", "parts/navbar.html"),
+    loadComponent("hero", "parts/hero.html"),
+    loadComponent("problem", "parts/problem.html"),
+    loadComponent("features", "parts/features.html"),
+    loadComponent("docs-preview", "parts/docs-preview.html"),
+    loadComponent("installation", "parts/installation.html"),
+    loadComponent("community", "parts/community.html"),
+    loadComponent("footer", "parts/footer.html"),
+  ]);
 
-loadComponent("navbar", "parts/navbar.html");
-loadComponent("hero", "parts/hero.html");
-loadComponent("problem", "parts/problem.html");
-loadComponent("features", "parts/features.html");
-loadComponent("docs-preview", "parts/docs-preview.html");
-loadComponent("installation", "parts/installation.html");
-loadComponent("community", "parts/community.html");
-loadComponent("footer", "parts/footer.html");
+  initThemeToggle();
+}
+
+InitPage();
