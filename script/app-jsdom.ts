d9a75609916dc7b07605d0445885c6f3a271160e
mmidtbo <mauza628@gmail.com>
2026-05-30 01:16:21 +0700

    chore(ts): debug jsdom

--- script/app-jsdom.ts
+++ script/app-jsdom.ts
@@ -1,4 +1,4 @@
-export default function initThemeToggle() {
+export default function initThemeToggle(): void {
   const btn = document.getElementById("themeToggle");
   const html = document.documentElement;
 
@@ -6,25 +6,50 @@ export default function initThemeToggle() {
     return;
   }
 
-  const stored = localStorage.getItem("data-theme") || "dark";
-  console.log("stored: ", stored);
-  if (stored === "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
-    btn.innerHTML = `<i class="bi bi-moon-stars-fill"></i>`;
-  }
+  const stored = localStorage.getItem("data-theme") || "light";
+  console.log(stored);
 
-  btn.addEventListener("click", () => {
+  html.setAttribute("data-theme", stored);
+  setLogo(stored);
+
+  btn.innerHTML =
+    stored === "dark"
+      ? `<i class="bi bi-moon-stars-fill"></i>`
+      : `<i class="bi bi-sunrise-fill"></i>`;
+
+  btn.addEventListener("click", (): void => {
     const isDark = html.getAttribute("data-theme");
     console.log("isDark: ", isDark);
     const next = isDark === "dark" ? "light" : "dark";
 
+    console.log(next);
+
+    // isdark old
+    // next new
+
     html.setAttribute("data-theme", next);
+    setLogo(next);
     localStorage.setItem("data-theme", next);
 
     btn.innerHTML =
       next === "dark"
         ? `<i class="bi bi-moon-stars-fill"></i>`
         : `<i class="bi bi-sunrise-fill"></i>`;
+  });
+}
+
+function setLogo(isDark: string): void {
+  console.log("exc");
+  console.log(isDark);
+
+  const src =
+    isDark === "dark" ? "./assets/logo.svg" : "./assets/logo-light.svg";
+  console.log(src);
+  document.querySelectorAll(".fl-logo-mark").forEach(function (img): void {
+    img.src = src;
+  });
+  document.querySelectorAll(".hero-logo").forEach(function (img): void {
+    img.src = src;
   });
 }
 
